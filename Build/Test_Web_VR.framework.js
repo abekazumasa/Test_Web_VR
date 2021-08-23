f4592b511fd382e53b30e3567db0a500f2cfe3f3
abekazumasa <kazumasa.danmaro@gmail.com>
2021-08-23 17:04:37 +0900

    ok_1

--- Build/Test_Web_VR.framework.js
+++ Build/Test_Web_VR.framework.js
@@ -2395,7 +2395,7 @@ function _emscripten_asm_const_ii(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 2465024;
+STATICTOP = STATIC_BASE + 2465600;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -4449,7 +4449,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 2465024;
+var STATIC_BUMP = 2465600;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
